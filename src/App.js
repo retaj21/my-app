dfefc13da949f41e2626b4ceef6f8319f35aa56f
retaj21 <retajabdelghani@gmail.com>
2025-07-22 13:41:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React</h1>
-    </div>
+    <div>
+<Header />
+</div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header =() =>{
+return(
+    <div>
+        <div>
+al salam 3alaikom
+        </div>
+    </div>
+);
+
+};
+export default Header;
